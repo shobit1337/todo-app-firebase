0f67cacbcdd42614e952d08f16a3c1529fc015c8
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-10 01:00:55 +0530

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/functions";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD-aO86MWraXkc3BWnwKraUoTxED3kT3Ys",
+  authDomain: "todo-app-neogcamp.firebaseapp.com",
+  projectId: "todo-app-neogcamp",
+  storageBucket: "todo-app-neogcamp.appspot.com",
+  messagingSenderId: "325578282408",
+  appId: "1:325578282408:web:f5c2a9b76cd43e2ccaf99d",
+  measurementId: "G-MTSPPZPW5L",
+});
