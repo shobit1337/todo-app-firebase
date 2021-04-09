edf4f2cdc357c67060de9450582c889fde87ba9e
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-10 00:22:38 +0530

    added the design of todo

---
+++ src/Todos.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+const Todos = () => {
+  const [todo, setTodo] = useState("");
+  const todos = [];
+};
+
+const signOut = () => {
+  const onSubmitTodo = (event) => {
+    event.preventDefault();
+    setTodo("");
+  };
+
+  return (
+    <>
+      <header>
+        <button onClick={SignOut}>Sign Out</button>
+      </header>
+      <main>
+        <form onSubmit={onSubmitTodo}>
+          <input
+            required
+            value={todo}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setTodo(e.target.value)
+            }
+            placeholder="What's Next?"
+          />
+          <button type="submit">Add</button>
+        </form>
+        {todos && todos.map(() => <Todo {...todo} />)}
+      </main>
+    </>
+  );
+};
+
+const Todo = ({ id, complete, text }) => {
+  const onCompleteTodo = (id, complete) => {};
+  const onDeleteTodo = (id) => {};
+
+  return (
+    <div key={id} className="todo">
+      <button
+        className={`todo-item ${complete ? "complete" : ""}`}
+        tabIndex="0"
+        onClick={() => onCompleteTodo(id, complete)}
+      >
+        {text}
+      </button>
+      <button onClick={() => onDeleteTodo(id)}>x</button>
+    </div>
+  );
+};
+
+export default Todos;
